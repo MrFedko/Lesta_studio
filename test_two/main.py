0fa76c46a223c4be24dbbf28e188542011afc751
MrFedko <miklerst@mail.ru>
2022-07-18 14:35:05 +0300

    create print result in main for first/second fifo

--- test_two/main.py
+++ test_two/main.py
@@ -9,3 +9,25 @@ if __name__ == '__main__':
     for i in range(length):
         first_queue.push(i + i)
         second_queue.push(i + i)
+
+    print(f"""first 
+{first_queue}""")
+
+    for i in range(5):
+        first_queue.pop()
+        first_queue.push(i + 100)
+        print(first_queue)
+
+    print("""
+    
+    --------------
+    
+    """)
+
+    print(f"""second 
+{second_queue}""")
+
+    for i in range(5):
+        second_queue.pop()
+        second_queue.push(i + 100)
+        print(second_queue)

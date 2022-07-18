a65425749217294386514ecd6adbd2d8d17d35f4
MrFedko <miklerst@mail.ru>
2022-07-18 14:16:22 +0300

    create getters/setters/__repr__ in Node for second fifo

--- test_two/fifo_second.py
+++ test_two/fifo_second.py
@@ -2,3 +2,22 @@ class Node:
     def __init__(self, next_element=None):
         self.data = 0
         self.next_element = next_element
+
+    @property
+    def next_element(self) -> int:
+        return self._next_element
+
+    @next_element.setter
+    def next_element(self, value: int):
+        self._next_element = value
+
+    @property
+    def data(self) -> int:
+        return self._data
+
+    @data.setter
+    def data(self, value: int):
+        self._data = value
+
+    def __repr__(self):
+        return str(self.data)
